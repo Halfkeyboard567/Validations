e08418c4229067185df69c1fe0e1ff050ac98aa6
Kimberly Rosa <rosa_106780@students.pupr.edu>
2023-09-27 13:30:49 -0500

    Delete Django to reinstall again

--- validations_app/transcript_validation/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin-page/', views.admin_page, name='admin-page'),
-    path('', views.login_view, name='login_view'),
-]
